fe4f2f9e769ff9f8a8c471d52e5de006b0e012d1
Paulo Bruny Lima <paulobruny@gmail.com>
2022-03-31 22:23:19 -0300

    set default styles and basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './Components/Home'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import Login from './Components/Login'
 
 const App = () => (
   <div>
-    Olar Doguinhos
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </div>
 )
 
