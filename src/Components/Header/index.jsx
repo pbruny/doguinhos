7becec0e01c52348e1e3415c07af92440a13a919
Paulo Bruny Lima <paulobruny@gmail.com>
2022-03-31 22:23:47 -0300

    create and style Header component

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Header.module.css'
+import { ReactComponent as Dogs } from '../../Assets/dogs.svg'
+
+const Header = () => (
+  <header className={styles.header}>
+    <nav className={`${styles.nav} container`}>
+      <Link className={styles.logo} to="/" aria-label="Doguinhos - Login">
+        <Dogs />
+      </Link>
+      <Link className={styles.login} to="/login">
+        Login / Criar
+      </Link>
+    </nav>
+  </header>
+)
+
+export default Header
