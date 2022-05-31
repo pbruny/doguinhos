46c2d82f2028298393b9dd34a3bcf8385e7e7a7d
Paulo Bruny Lima <paulobruny@gmail.com>
2022-05-31 20:35:43 -0300

    create only markup for logged area components

--- src/Components/User/UserLoggedArea/index.jsx
+++ src/Components/User/UserLoggedArea/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Feed from '../../Feed'
+import UserHeader from '../UserHeader'
+import UserPhotoPost from '../UserPhotoPost'
+import UserStats from '../UserStats'
 
 const UserLoggedArea = () => {
   return (
-    <div>UserLoggedArea</div>
+    <section className='container'>
+      <UserHeader />
+      <Routes>
+        <Route path='/' element={<Feed />} />
+        <Route path='/post' element={<UserPhotoPost />} />
+        <Route path='/stats' element={<UserStats />} />
+      </Routes>
+    </section>
   )
 }
 
