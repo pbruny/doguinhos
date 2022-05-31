806053d769fd7f50eb38da9e03eed63ecdd4635e
Paulo Bruny Lima <paulobruny@gmail.com>
2022-05-31 19:24:26 -0300

    create protected route to protect logged area

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Footer from './Components/Footer'
 import Header from './Components/Header'
 import Login from './Components/Login'
 import { UserStorage } from './Contexts/UserContext'
+import UserLoggedArea from './Components/User/UserLoggedArea'
+import ProtectedRoute from './Helper/ProtectedRoute'
 
 const App = () => (
   <div>
@@ -14,6 +16,14 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login/*" element={<Login />} />
+          <Route 
+            path="/conta/*" 
+            element={
+            <ProtectedRoute>
+              <UserLoggedArea />
+            </ProtectedRoute>
+            } 
+          />
         </Routes>
         <Footer />
 
---
+++ src/Components/User/UserLoggedArea/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserLoggedArea = () => {
+  return (
+    <div>UserLoggedArea</div>
+  )
+}
+
+export default UserLoggedArea
