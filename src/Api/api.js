06cad8832dad1872c826fef0bd53ba619d4e035b
Paulo Bruny Lima <paulobruny@gmail.com>
2022-06-01 20:13:23 -0300

    add single photo get

--- src/Api/api.js
+++ src/Api/api.js
@@ -48,3 +48,23 @@ export const PHOTO_POST = (formData, token) => {
     },
   }
 }
+
+export const PHOTOS_GET = ({page, total, user}) => {
+  return {
+    url: `${apiUrl}/api/photo?_page=${page}&_total=${total}&_user=${user}`,
+    options: {
+      method: 'GET',
+      cache: 'no-store'
+    },
+  }
+}
+
+export const PHOTO_GET = (id) => {
+  return {
+    url: `${apiUrl}/api/photo/${id}`,
+    options: {
+      method: 'GET',
+      cache: 'no-store'
+    },
+  }
+}
