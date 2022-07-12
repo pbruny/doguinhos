8eff7012066653dca7768654d46f5d31a012fa52
Paulo Bruny Lima <paulobruny@gmail.com>
2022-07-12 19:25:02 -0300

    add footer styles and fix container height

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,7 +1,12 @@
-import styles from './Footer.module.css'
+import React from 'react';
+import styles from './Footer.module.css';
+import {ReactComponent as Dogs} from '../../Assets/dogs-footer.svg';
 
 const Footer = () => (
-  <footer className={styles.footer}>Footer</footer>
+  <footer className={styles.footer}>
+    <Dogs />
+    <p>Doguinhos maravilhosos</p>
+  </footer>
 )
 
 export default Footer
